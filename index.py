a25716592e4b4fcb9f2ee9ec8159abc4d6f7fabc
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-11 10:26:12 -0400

    mvp commit for math expressions in python

---
+++ index.py
@@ -0,0 +1,49 @@
+# Rewrite this so total equals 10.
+total = "4" + 6
+total = 4 + 6
+
+# What is the value of third_number?
+first_number = 5
+second_number = 6
+third_number = first_number * second_number
+print(third_number)
+# 11
+
+# Divide 12 by the decimal .5 and assign the result to the variable outcome
+outcome = 12/ .5
+
+# Divide the variable total by 3 and assign the result to the variable each_share.
+each_share = total / 3
+
+# Subtract one number from another and assign the result to a variable. Make up the numbers and the variable name.
+freight = 1 - 1
+
+# Divide one variable by another and assign the result to a third variable. Make up the variable names.
+time = 10
+cost = 10
+total = time / cost
+
+# Subtract a variable from a number and assign the result to a second variable. Make up the variable names and the number.
+var1 = 5
+random_number = var1 - 10
+
+# Multiply a variable by itself and assign the result to the same variable. Make up the variable name.
+multi1 = 25
+multi1 = multi1 * multi1
+
+# Multiply three numbers and assign the result to a variable. Make up the numbers and the variable name.
+multiple_nums = 3 * 3 * 3
+
+# In one line of code, display the result of multiplying two variables. Make up the variable names.
+print(time * cost)
+
+# Assign a number to a variable, and another number to a second variable. Multiply the two, using the variables. Assign the result to a third variable. Display the result using the third variable.
+
+varry_one = 10
+varry_two = 20
+varry_three = varry_one * varry_two
+print(varry_three)
+#200
+
+# In a single line of code, divide one number by another and display the result.
+print(10 / 5)
